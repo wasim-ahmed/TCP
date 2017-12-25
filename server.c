f127445de3db7b8d83536e754d7715ec906c2a44
Wasim <wasim_miet@yahoo.in>
2017-12-25 15:57:21 +0530

    updated the server code for knowing Client ip address

--- server.c
+++ server.c
@@ -3,16 +3,24 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <string.h>
 #define PORT 8080
+
+
 int main(int argc, char const *argv[])
 {
     int server_fd, new_socket, valread;
-    struct sockaddr_in address;
+    struct sockaddr_in address,cli_address;
     int opt = 1;
     int addrlen = sizeof(address);
+    int cli_addrlen = sizeof(cli_address);
     char buffer[1024] = {0};
     char *hello = "Hello from server";
+
+	char clntName[INET_ADDRSTRLEN];
+	FILE *output;
+
       
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -44,15 +52,37 @@ int main(int argc, char const *argv[])
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&cli_address, 
+                       (socklen_t*)&cli_addrlen))<0)
     {
         perror("accept");
         exit(EXIT_FAILURE);
     }
+
+
+	printf("Client ip address:%s",inet_ntoa(cli_address.sin_addr));
+	printf("\t");
+	printf("Client port:%d",ntohs(cli_address.sin_port));
+	printf("\n");
+
+
+/*
+
+if(inet_ntop(AF_INET,&cli_address.sin_addr.s_addr,clntName,sizeof(clntName))!=NULL){
+   output = fopen("output.txt","a+");  
+   fprintf(output,"%s%c%d",clntName,'/',ntohs(cli_address.sin_port));  
+   fclose(output);
+} else {
+   printf("Unable to get address\n"); // i just fixed this to printf .. i had it as print before
+}
+
+*/
+
     valread = read( new_socket , buffer, 1024);
     printf("%s\n",buffer );
     send(new_socket , hello , strlen(hello) , 0 );
     printf("Hello message sent\n");
     return 0;
 }
+
+
